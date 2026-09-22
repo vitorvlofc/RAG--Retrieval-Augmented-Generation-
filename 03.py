8d3a6675dd98be0f61fc8f914d632f5395a17514
vitor <vitorvlofc@users.noreply.github.com>
2026-09-22 16:14:45 -0300

    033 - Usando vector store persistido

---
+++ 03.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+load_dotenv()
+
+model = ChatOpenAI(
+    model="gpt-4o"
+)
+
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vectorstore = Chroma(
+    persist_directory="./chroma_db",
+    embedding_function=embedding,
+    collection_name="manual_notebook"
+)
+
+retriever = vectorstore.as_retriever()
+
+system_prompt = """
+Use o contexto para responder às perguntas.
+
+Contexto:
+{context}
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{question}")
+    ]
+)
+
+chain = (
+    {
+        "context": retriever,
+        "question": RunnablePassthrough()
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+query = "Qual a capacidade da bateria do notebook?"
+response = chain.invoke(query)
+
+print(response)
